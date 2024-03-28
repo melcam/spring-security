a422b997c9da9eeed7a4b8d8c9fe9ef2ae81e063
melcam <yumelcam.93@gmail.com>
2024-03-28 17:54:48 +0100

    diferente peticion con roles de autorizacion asignado

--- src/main/java/com/melcam/app/controller/TestController.java
+++ src/main/java/com/melcam/app/controller/TestController.java
@@ -1,30 +1,40 @@
 package com.melcam.app.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @PreAuthorize("denyAll()")
 public class TestController {
 
-    @GetMapping("/hello")
-    @PreAuthorize("permitAll()")
-    public String hello() {
-        return "Welcome";
-    }
-
-    @GetMapping("/hello-secured")
+    @GetMapping("/get")
     @PreAuthorize("hasAuthority('READ')")
-    public String helloSecured() {
-        return "Welcome secured";
+    public String helloGet() {
+        return "Welcome - GET";
     }
 
-    @GetMapping("/hello-secured2")
-    @PreAuthorize("hasAuthority('CREATE')")
-    public String helloSecured2() {
-        return "Welcome secured2";
+    @PostMapping("/post")
+    @PreAuthorize("hasAuthority('CREATE') or hasAuthority('READ')")
+    public String helloPost() {
+        return "Welcome - POST";
+    }
+
+    @PutMapping("/put")
+    @PreAuthorize("hasAuthority('UPDATE')")
+    public String helloPut() {
+        return "Welcome - PUT";
+    }
+
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasAuthority('DELETE')")
+    public String helloDelete() {
+        return "Welcome - DELETE";
+    }
+
+    @PatchMapping("/patch")
+    @PreAuthorize("hasAuthority('REFACTOR')")
+    public String helloPatch() {
+        return "Welcome - PATCH";
     }
 }
